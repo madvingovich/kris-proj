9872e2b26b7392f2cb1234bd20af0cf11a5ecaae
vbivtsya <vadvoker@gmail.com>
2019-03-30 12:51:26 +0200

    ok

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,56 @@
 (function ($) {
-    console.log('--->', 1);
-    console.log('--->', $('h1'));
+    let
+        modal = $('.modal'),
+        modalContent = modal.find('.content'),
+        pfImg = $('.photo img'),
+        serImg = $('.service-item img');
+
+    makePfImagesHeight.call(pfImg);
+    makePfImagesHeight.call(serImg);
+
+
+    if($('.main').hasClass('active')) {
+        $(window).on('resize', function() {
+            makePfImagesHeight.call(pfImg)
+        });
+    } else if($('.services').hasClass('active')) {
+        $(window).on('resize', function() {
+            makePfImagesHeight.call(serImg)
+        });
+    }
+
+    pfImg.on('click', function(e) {
+        openModal(e);
+    });
+    $('.service-item').on('click',function(e) {
+        openModal(e);
+    });
+
+    modal.on('click', function(e) {
+        if(!e.target.closest('img') && !e.target.closest('.contact-me')) {
+            modal.removeClass('active');
+            modalContent.removeClass('active');
+        }
+    });
+    
+    $(modal).on('mousewheel', function(e) {
+        e.preventDefault();
+    });
+
+
+
+    function openModal(e) {
+        if($(window).width() < 450 && e.target.closest('.photo')) return;
+        modal.addClass('active');
+        if(e.target.closest('.photo')) {
+            modalContent.find('img').attr('src', $(e.target).attr('src'));
+        }
+        setTimeout(function() {
+            modalContent.addClass('active')
+        }, 120);
+    }
+
+    function makePfImagesHeight() {
+        this.height(this.width() * .573);
+    }
 })(jQuery);
